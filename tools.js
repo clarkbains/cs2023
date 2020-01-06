db77d76e8341bc23afd9564c30876f672fe5989d
Clark Bains <clarkbains@gmail.com>
2020-01-06 12:37:02 -0500

    Started Adding Webdev tools

---
+++ tools.js
@@ -0,0 +1,23 @@
+function encode (inID, outID) {
+    var inElm = document.getElementById(inID)
+    var outElm = document.getElementById(outID);
+    outElm.value = encodeURIComponent(inElm.value)
+}
+function copy (elmID) {
+    textarea = document.createElement('textarea')
+    textarea.textContent =  document.getElementById(elmID).value;
+    textarea.style.position = "fixed";
+    document.body.appendChild(textarea);
+    textarea.select();
+    try {
+        return document.execCommand("copy");
+    }
+    catch (ex) {
+        console.warn("Copy to clipboard failed.", ex);
+        return false;
+    }
+    finally {
+        document.body.removeChild(textarea);
+    }
+}
+ x = document.getElementById("btn encode")
